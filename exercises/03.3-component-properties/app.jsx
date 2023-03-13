939eb600966660e645504eee251de99d2f43a794
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-03-13 18:27:32 +0000

    listoco

--- exercises/03.3-component-properties/app.jsx
+++ exercises/03.3-component-properties/app.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropType from "prop-types";
+import PropTypes from "prop-types";
 
-const BootstrapCard = props => {
+const BootstrapCard = (props) => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
@@ -18,14 +18,22 @@ const BootstrapCard = props => {
 	);
 };
 BootstrapCard.propTypes = {
-	title: PropType.string,
-		// 2) add here the new properties into the proptypes object
-	imageUrl: PropType.string,
-	description: PropType.string,
-	buttonUrl: PropType.string,
-	buttonLabel: PropType.string,
+	title: PropTypes.string,
+	// 2) add here the new properties into the proptypes object
+	imageUrl: PropTypes.string,
+	description: PropTypes.string,
+	buttonUrl: PropTypes.string,
+	buttonLabel: PropTypes.string,
 };
 
-
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
-ReactDOM.render(<BootstrapCard buttonLabel="Go to wikipedia" buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan" description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter." title="Bob Dylan" imageUrl={"https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"}/>, document.querySelector("#myDiv"));
+ReactDOM.render(
+	<BootstrapCard
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+		title="Bob Dylan"
+		imageUrl={"https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"}
+	/>,
+	document.querySelector("#myDiv")
+);
